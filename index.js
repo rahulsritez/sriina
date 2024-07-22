c012164fe08efa162b53c61cd0c5505653505034
rahulsritez <59550832+rahulsritez@users.noreply.github.com>
2024-07-22 17:59:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.use(function (req, res, next) {
 });
 
 // all environments
-const port = 3000;
+const port = 80;
 app.set("views", __dirname + "/views");
 //app.set('common', __dirname + '/common');
 app.set("view engine", "ejs");
